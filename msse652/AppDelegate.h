af5bb640e7e3e209a59e4ba476a632c3e599ae47
PT <PT@PTs-MacBook-Pro.local>
2015-06-29 19:05:49 -0600

    Initial Commit

---
+++ msse652/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  msse652
+//
+//  Created by PT on 6/29/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ msse652/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  msse652
+//
+//  Created by PT on 6/29/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
